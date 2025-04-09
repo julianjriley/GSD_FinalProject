307559b8a787492b787b48e76b35fca7ac7d38d3
ufl-detherage <etheraged@ufl.edu>
2025-04-09 16:23:00 -0400

    Added Player Functionality

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSetup : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    Camera _camera;
+    public void IsLocalPlayer()
+    {
+        movement.enabled = true;
+        _camera = Camera.main;
+        _camera.gameObject.SetActive(true);
+    }
+
+}
