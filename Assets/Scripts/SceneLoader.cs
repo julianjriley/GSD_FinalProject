c504998ec8426bfbba766668c0a407ccb53715c2
Andrew Shen <Shenking21@gmail.com>
2025-04-15 15:41:06 -0400

    Menu Scene Changes
    Added A Menu scene with a load scene and quit button. Not much else to it.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadSceneByName()
+    {
+        SceneManager.LoadScene("SceneNameHere");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("The Game will end in the actual build");
+        Application.Quit();
+    }
+
+}
