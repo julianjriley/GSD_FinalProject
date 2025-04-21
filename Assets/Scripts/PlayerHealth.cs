49a8cbde0cd949b08ab71a4e679375be33cdd682
TheFHazelnut <02enzio@gmail.com>
2025-04-21 15:37:27 -0400

    HELP

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -8,16 +8,26 @@ public class PlayerHealth : MonoBehaviour
 {
     public static event Action<float> HealthChanged;
 
+    PhotonView pv;
+
     public float maxHealth = 5;
     float health;
 
+    private void OnEnable()
+    {
+        pv = GetComponent<PhotonView>();
+    }
+
     private void Start()
     {
-        health = maxHealth;
+        if(pv.IsMine)
+            health = maxHealth;
     }
 
     public void TakeDamage(float damage)
     {
+        if (!pv.IsMine)
+            return;
         health -= damage;
         if (health < 0 )
         {
